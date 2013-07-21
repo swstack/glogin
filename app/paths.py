93fff69e8f88d6e48f4d436221d69ad246c35166
Stephen Stack <stephen.stack@spectrumdsi.com>
2013-07-21 00:47:16 -0500

    first commit

---
+++ app/paths.py
@@ -0,0 +1,10 @@
+import os
+
+
+ROOT = os.path.join(os.path.dirname(__file__), "../")
+
+PROJECT_ROOT = os.path.join(ROOT, "app")
+
+TEMPLATES = os.path.join(os.path.dirname(__file__), "templates")
+
+PATH_CLIENT_SECRETS = os.path.join(PROJECT_ROOT, "client_secrets.json")
